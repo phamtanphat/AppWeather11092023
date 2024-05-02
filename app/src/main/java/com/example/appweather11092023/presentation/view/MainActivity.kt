80a6355bc4ae4e67bd90fc2db9006d035b5dc752
phamtanphat <phatdroid94@gmail.com>
2024-05-02 20:50:08 +0700

    ui

--- app/src/main/java/com/example/appweather11092023/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/appweather11092023/presentation/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.appweather11092023
+package com.example.appweather11092023.presentation.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.appweather11092023.R
 import com.example.appweather11092023.common.AppInterface
 import com.example.appweather11092023.data.repository.WeatherRepository
 
---
+++ app/src/main/java/com/example/appweather11092023/presentation/viewmodel/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.appweather11092023.presentation.viewmodel
+
+import com.example.appweather11092023.data.repository.WeatherRepository
+
+class MainViewModel {
+    private var weatherRepository = WeatherRepository()
+
+    
+}
